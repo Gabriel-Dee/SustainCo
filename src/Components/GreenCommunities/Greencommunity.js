ca6e194297d7d286f9a3bfcbcb4240d818446ced
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-03-30 18:05:17 +0300

    changed map styling

--- src/Components/GreenCommunities/Greencommunity.js
+++ src/Components/GreenCommunities/Greencommunity.js
@@ -5,13 +5,15 @@ function Greencommunity() {
     <div className="green-com">
       <h1>Locate Your Community</h1>
       <p> </p>
-      <div class="mapouter">
-        <div class="gmap_canvas">
-          <iframe width="1014" height="616" id="gmap_canvas" src="https://maps.google.com/maps?q=Google%20Headquarters&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-
-          </iframe>
-          </div>
-          </div>
+      <iframe
+        style={{width:"100%", height:"600px"}}
+        id="gmap_canvas"
+        src="https://maps.google.com/maps?q=Google%20Headquarters&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
     </div>
   );
 }
